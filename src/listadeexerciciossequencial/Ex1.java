76f692033c932912186859eeea295d1a0446dc3e
MatSchubert84 <mateuscamargoschubert@gmail.com>
2023-04-03 22:03:40 -0300

    Exercicio1

---
+++ src/listadeexerciciossequencial/Ex1.java
@@ -0,0 +1,10 @@
+package listadeexerciciossequencial;
+
+public class Ex1 {
+
+    public static void main(String[] args) {
+
+        //Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+        System.out.println("Hello World");
+    }
+}
